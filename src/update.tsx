accdd312bacc2476e990f0fb8beb706fd16b9e7d
sivaraman <sivaraman.p@fr8.in>
2024-02-29 10:48:20 +0530

    ver 7

--- src/update.tsx
+++ src/update.tsx
@@ -3,17 +3,18 @@ import { SplashScreen } from '@capacitor/splash-screen'
 import { App } from '@capacitor/app'
 
 const InitializeApp= ()=> {
-  console.log("ver 6")
+  console.log("ver 7")
 let data:any
 CapacitorUpdater.notifyAppReady()
 App.addListener('appStateChange', async(state) => {
      if (state.isActive) {
        // Do the download during user active app time to prevent failed download
        data = await CapacitorUpdater.download({
-       version: '6.0.0',
-       url: 'https://github.com/Sivaraman1530/sample-test/archive/refs/tags/test_sample.zip',
+       version: '7.0.0',
+       url: 'https://github.com/Sivaraman1530/sample-test/archive/refs/heads/master.zip',
        })
      }
+     console.log("data",JSON.stringify(data))
      if (!state.isActive && data.version !== "") {
        // Do the switch when user leave app
        SplashScreen.show()
